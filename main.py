7e7df58911542343f33182b8a0dfb3a77e1af4e9
Zach <zachtvin@gmail.com>
2026-05-09 13:31:41 -0400

    seems to work

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import torch.nn as nn
 import torch
+from torch import optim
 import torch.functional as F
 import numpy as np
 import gymnasium as gym
 from collections import deque
 from dataclasses import dataclass
+import random
 
 class PPOAgent():
     # has network and device
@@ -38,8 +40,8 @@ class D4PGCritic(nn.Module):
         
         self.use_target = use_target
         if self.use_target:
-            self.target = DDPGActor(obs_size, act_size, n_atoms, v_min, v_max, 
-                                    use_target, False)
+            self.target = D4PGCritic(obs_size, act_size, n_atoms, v_min, v_max, 
+                                     False)
 
     def forward(self, x: torch.Tensor, a: torch.Tensor):
         obs = self.obs_net(x)
@@ -51,6 +53,17 @@ class D4PGCritic(nn.Module):
         weights = F.softmax(distr, dim=1) * self.supports
         res = weights.sum(dim=1)
         return res.unsqueeze(dim=-1)
+    
+    def sync(self, a):
+        assert self.use_target
+
+        state = self.state_dict()
+        target_state = self.target.state_dict()
+        for k, v in state.items():
+            target_state[k] = target_state[k] * a + (1-a) * v
+
+        self.target.load_state_dict(target_state)
+
     
 class D4PGAgent():
     def __init__(self, actor, device, epsilon):
@@ -163,10 +176,9 @@ class ExperienceFL:
     reward: float
     final: np.ndarray
 
-class ExperienceSourceFL():
-    def __init__(self, env, agent, steps_count, steps_delta, gamma):
-        super(ExperienceSourceFL, self).__init__(env, agent, steps_count+1,
-                                                 steps_delta)
+class ExperienceSourceFL(ExperienceSource):
+    def __init__(self, env, agent, steps_count, gamma):
+        super(ExperienceSourceFL, self).__init__(env, agent, steps_count+1)
         self.gamma = gamma # reward discount
         self.steps = steps_count
 
@@ -181,7 +193,7 @@ class ExperienceSourceFL():
             # otherwise, calculate reward based on all but last state
             # and last state is returned to be used for bootstrapping
             else:
-                last_state = mem[-1]
+                last_state = mem[-1].state
                 items = mem[:-1]
 
             # calculate discounted reward backward
@@ -190,7 +202,59 @@ class ExperienceSourceFL():
                 discounted_reward *= self.gamma
                 discounted_reward += m.reward
 
-            yield ExperienceFL(state=mem[0], action=action, reward=discounted_reward, final=last_state)
+            yield ExperienceFL(state=mem[0].state, action=mem[0].action, reward=discounted_reward, final=last_state)
+
+class ExperienceReplayBuffer():
+    def __init__(self, exp_source, buffer_size):
+        self.exp_iterative = iter(exp_source)
+        self.buffer = []
+        self.capacity = buffer_size
+        self.idx = 0
+
+    def __len__(self):
+        return len(self.buffer)
+    
+    def __iter__(self):
+        return iter(self.buffer)
+    
+    def sample(self, batch_size):
+        if len(self.buffer <= batch_size):
+            return self.buffer
+        
+        idxs = np.random.choice(len(self.buffer), replace=True)
+        return [self.buffer[idx] for idx in idxs]
+    
+    def _add(self, sample):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(sample)
+        else:
+            self.buffer[self.idx] = sample
+
+        self.idx = (self.idx + 1) % self.capacity
+
+    def populate(self, samples):
+        for _ in range(samples):
+            exp = next(self.exp_iterative)
+            self._add(exp)
+
+def unpack_batch(batch, device="cpu"):
+    states, actions, rewards, dones, last_states = [], [], [], [], []
+    for exp in batch:
+        states.append(exp.state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        dones.append(exp.last_state is None)
+        if exp.last_state is None:
+            last_states.append(exp.state)
+        else:
+            last_states.append(exp.last_state)
+    states_v = torch.as_tensor(np.array(states, dtype=np.float32)).to(device)
+    actions_v = torch.as_tensor(np.array(actions, dtype=np.float32)).to(device)
+    rewards_v = torch.as_tensor(np.array(rewards, dtype=np.float32)).to(device)
+    last_states_v = torch.as_tensor(np.array(last_states, dtype=np.float32)).to(device)
+    dones_t = torch.BoolTensor(dones).to(device)
+    return states_v, actions_v, rewards_v, dones_t, last_states_v
+
 
 def distr_proj(next_distr, rewards, dones, gamma):
     batch_size = len(rewards)
@@ -243,19 +307,20 @@ def distr_proj(next_distr, rewards, dones, gamma):
 
     return proj_distr
 
+GAMMA = 0.99 # reward discounting
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
+REPLAY_SIZE = 100000
+REPLAY_INITIAL = 10000
+REWARD_STEPS = 5
+TEST_ITERS = 1000
+Vmax = 10
+Vmin = -10
+N_ATOMS = 51 # atoms for Q distribution
+EPSILON = 0.3 # noise
+DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
+
 if __name__ == "__main__":
-    GAMMA = 0.99 # reward discounting
-    BATCH_SIZE = 64
-    LEARNING_RATE = 1e-4
-    REPLAY_SIZE = 100000
-    REPLAY_INITIAL = 10000
-    REWARD_STEPS = 5
-    TEST_ITERS = 1000
-    Vmax = 10
-    Vmin = -10
-    N_ATOMS = 51 # atoms for Q distribution
-    EPSILON = 0.3 # noise
-    DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
 
     env = gym.make('Ant-v5', render_mode='human')
     critic = D4PGCritic(env.observation_space.shape[0],
@@ -266,44 +331,67 @@ if __name__ == "__main__":
     
     agent = D4PGAgent(actor, torch.device('cuda' if torch.cuda.is_available() else 'cpu'), EPSILON)
 
-    ep = 0
+    exp_source = ExperienceSourceFL(env, agent, steps_count=REWARD_STEPS, gamma=GAMMA)
+    buffer = ExperienceReplayBuffer(exp_source, buffer_size=REPLAY_SIZE)
+
+    actor_optimizer = optim.Adam(actor.parameters(), lr=LEARNING_RATE)
+    critic_optimizer = optim.Adam(critic.parameters(), lr=LEARNING_RATE)
+
+    device = torch.device('gpu' if torch.cuda.is_available() else 'cpu')
+
+    step = 0
+    best_rew = None
     while True:
-        
-        obs, _ = env.reset()
-        term, trunc = False, False
+        print(f'Step {step}')
+        step += 1
+        buffer.populate(1)
 
-        ep_rews = []
-
-        steps = 0
-        reward_group = []
-        while not term or trunc:
-            action = env.action_space.sample()
-
-            obs, rew, term, trunc, _ = env.step(action)
-
-            ep_rews.append(rew)
-
-            reward_group.append([obs, rew, term or trunc])
-            if steps % REWARD_STEPS == 0:
-                # work backwards to get rewards
-                discounted_reward = 0
-                done_states = []
-                for i,item in enumerate(reversed(reward_group)):
-                    discounted_reward *= GAMMA
-                    discounted_reward += reward_group[1]
-
-                # mask by done states and add + scale to critic output
-
-            steps += 1
-
-        print(f'Ep {ep}: {np.array(ep_rews).mean()}')
+        # keep going until buffer is full
+        if len(buffer) < REPLAY_INITIAL:
+            continue
 
         batch = buffer.sample(BATCH_SIZE)
-        # get all state info
-        # work backwards, unroll N rewards with critic Q value summed
 
-        ep += 1
-        break
+        states, actions, rews, dones, last_states = unpack_batch(batch, device)
 
+        print(rews)
+
+        # critic > add discounted critic output for last state
+        critic_optimizer.zero_grad()
+        critic_out = critic(states, actions)
+        
+        last_actions = actor.target(last_states)
+        last_distr = F.softmax(critic.target(last_states, last_actions), dim=1)
+
+        # target distribution
+        proj_distr = distr_proj(last_distr.detach().cpu().numpy(),
+                                rews.detach().cpu().numpy(),
+                                dones.detach().cpu().numpy(),
+                                GAMMA**REWARD_STEPS)
+        proj_distr_tensor = torch.tensor(proj_distr).to(device)
+
+        # log_softmax critic output to form prrobability distribution
+        # mulitply by target distribution to get expected reward
+        prob_distr_tensor = -F.log_softmax(critic_out, dim=1) * proj_distr_tensor
+        critic_loss_tensor = prob_distr_tensor.sum(dim=1).mean()
+        critic_loss_tensor.backward()
+        critic_optimizer.step()
+
+        # actor > optimize negative critic
+        actor_optimizer.zero_grad()
+        
+        actor_actions = actor(states)
+        critic_out = critic(states, actor_actions)
+
+        # collapse distribution to one q value
+        actor_loss = -critic.distr_to_q(critic_out)
+        actor_loss.backward()
+        actor_optimizer.step()
+
+        # update target networks with alpha=1-1e-3
+        actor.sync(1 - 1e-3)
+        critic.sync(1 - 1e-3)
+
+        
     print('Training done.')
     env.close()
